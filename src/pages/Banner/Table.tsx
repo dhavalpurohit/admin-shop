71bf5291dc1c1dd730de259253f06bfe8830a740
Pratap Solanki <solankipratap.work@gmail.com>
2024-10-10 23:35:25 +0530

    feat: banner list ui

---
+++ src/pages/Banner/Table.tsx
@@ -0,0 +1,309 @@
+import { useState } from 'react';
+import { Banner } from '../../types/banner';
+import DropDownCommon from '../../components/DropDownCommon';
+import { NavLink } from 'react-router-dom';
+import search from '../../../src/images/common/search.svg';
+
+const bannerData: Banner[] = [
+  {
+    title: 'Samsung Galaxy S21',
+    subtitle: 'Subtitle: Track Deals, Pricing, and Inventory for this Item ',
+    brand: 'Samsung',
+    category: 'Electronics',
+    status: 'QC Passed',
+    type: 'Product management',
+    max_min_price: [100, 200],
+    image: 'abc.svg',
+  },
+];
+
+// const singleUploadData = [
+//   {
+//     product_name: 'Abc',
+//     product_id: '1',
+//     brand: 'Ajio',
+//     price: 200,
+//     quantity: 1,
+//     date: '12/31/2023 10:00 AM',
+//   },
+// ];
+
+const category = [
+  {
+    name: 'cate 1',
+    value: 'cate 1',
+  },
+  {
+    name: 'cate 2',
+    value: 'cate 2',
+  },
+];
+const status = [
+  {
+    name: 'Active',
+    value: 'active',
+  },
+  {
+    name: 'Pending',
+    value: 'Pending',
+  },
+];
+const type = [
+  {
+    name: 'type 1',
+    value: 'type 1',
+  },
+  {
+    name: 'type 2',
+    value: 'type 2',
+  },
+];
+
+const BannerTable: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  return (
+    <div className="">
+      <div className="flex items-center bg-white justify-between p-4 rounded-xl border border-stroke  shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex items-center gap-2.5">
+          <div className="w-full">
+            <label
+              className="mb-3 block text-sm font-medium text-black dark:text-white"
+              htmlFor="category"
+            >
+              Category
+            </label>
+            <div className="relative">
+              <DropDownCommon
+                lists={category}
+                labelKey="name"
+                valueKey="value"
+              />
+            </div>
+          </div>
+          <div className="w-full">
+            <label
+              className="mb-3 block text-sm font-medium text-black dark:text-white"
+              htmlFor="category"
+            >
+              Status
+            </label>
+            <div className="relative">
+              <DropDownCommon lists={status} labelKey="name" valueKey="value" />
+            </div>
+          </div>
+          <div className="w-full">
+            <label
+              className="mb-3 block text-sm font-medium text-black dark:text-white"
+              htmlFor="category"
+            >
+              Type
+            </label>
+            <div className="relative">
+              <DropDownCommon lists={type} labelKey="name" valueKey="value" />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-end gap-4.5">
+          <NavLink
+            className="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
+            to={'/add-banner'}
+          >
+            Add Banner
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="flex items-center bg-white justify-between p-4 rounded-t-xl mt-2">
+        <div className="relative w-72 ml-auto">
+          {/* Search Icon */}
+          <div
+            className={`absolute left-2.5 top-1/2 transform -translate-y-1/2 transition-all duration-300 ease-in-out ${
+              isFocused || query
+                ? '-translate-x-6 opacity-0'
+                : 'translate-x-0 opacity-100'
+            }`}
+          >
+            <img src={search} alt={`image`} />
+          </div>
+
+          {/* Search Input */}
+          <input
+            type="text"
+            value={query}
+            onChange={handleInputChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            placeholder="Search..."
+            className="w-full pl-8 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500 transition-all duration-300 ease-in-out"
+          />
+        </div>
+      </div>
+      <div className="bg-white">
+        <div className="max-w-full overflow-x-auto">
+          <table className="w-full table-auto">
+            <thead>
+              <tr className="bg-gray-2 text-left dark:bg-meta-4">
+                <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                  Title
+                </th>
+                <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
+                  Sub Title
+                </th>
+                <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                  Brand
+                </th>
+                <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                  Category
+                </th>
+                <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                  Type
+                </th>
+                <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                  maximum and Minimum price
+                </th>
+
+                <th className="py-4 px-4 font-medium text-black dark:text-white">
+                  Status
+                </th>
+                <th className="py-4 px-4 font-medium text-black dark:text-white">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {bannerData.map((item, key) => (
+                <tr key={key}>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="text-black dark:text-white">{item.title}</p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="text-black dark:text-white">
+                      {item.subtitle}
+                    </p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="text-black dark:text-white">{item.brand}</p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="text-black dark:text-white">
+                      {item.category}
+                    </p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="text-black dark:text-white">{item.type}</p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="text-black dark:text-white">
+                      {item.max_min_price
+                        ? `min: ${item.max_min_price[0]}, max: ${item.max_min_price[1]}`
+                        : 'Price not available'}
+                    </p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p
+                      className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${
+                        item.status === 'Paid'
+                          ? 'bg-success text-success'
+                          : item.status === 'Unpaid'
+                          ? 'bg-danger text-danger'
+                          : 'bg-warning text-warning'
+                      }`}
+                    >
+                      {item.status}
+                    </p>
+                  </td>
+
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <div className="flex items-center space-x-3.5">
+                      <button className="hover:text-primary">
+                        <svg
+                          className="fill-current"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z"
+                            fill=""
+                          />
+                          <path
+                            d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z"
+                            fill=""
+                          />
+                        </svg>
+                      </button>
+                      {/* <button className="hover:text-primary">
+                        <svg
+                          className="fill-current"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M13.7535 2.47502H11.5879V1.9969C11.5879 1.15315 10.9129 0.478149 10.0691 0.478149H7.90352C7.05977 0.478149 6.38477 1.15315 6.38477 1.9969V2.47502H4.21914C3.40352 2.47502 2.72852 3.15002 2.72852 3.96565V4.8094C2.72852 5.42815 3.09414 5.9344 3.62852 6.1594L4.07852 15.4688C4.13477 16.6219 5.09102 17.5219 6.24414 17.5219H11.7004C12.8535 17.5219 13.8098 16.6219 13.866 15.4688L14.3441 6.13127C14.8785 5.90627 15.2441 5.3719 15.2441 4.78127V3.93752C15.2441 3.15002 14.5691 2.47502 13.7535 2.47502ZM7.67852 1.9969C7.67852 1.85627 7.79102 1.74377 7.93164 1.74377H10.0973C10.2379 1.74377 10.3504 1.85627 10.3504 1.9969V2.47502H7.70664V1.9969H7.67852ZM4.02227 3.96565C4.02227 3.85315 4.10664 3.74065 4.24727 3.74065H13.7535C13.866 3.74065 13.9785 3.82502 13.9785 3.96565V4.8094C13.9785 4.9219 13.8941 5.0344 13.7535 5.0344H4.24727C4.13477 5.0344 4.02227 4.95002 4.02227 4.8094V3.96565ZM11.7285 16.2563H6.27227C5.79414 16.2563 5.40039 15.8906 5.37227 15.3844L4.95039 6.2719H13.0785L12.6566 15.3844C12.6004 15.8625 12.2066 16.2563 11.7285 16.2563Z"
+                            fill=""
+                          />
+                          <path
+                            d="M9.00039 9.11255C8.66289 9.11255 8.35352 9.3938 8.35352 9.75942V13.3313C8.35352 13.6688 8.63477 13.9782 9.00039 13.9782C9.33789 13.9782 9.64727 13.6969 9.64727 13.3313V9.75942C9.64727 9.3938 9.33789 9.11255 9.00039 9.11255Z"
+                            fill=""
+                          />
+                          <path
+                            d="M11.2502 9.67504C10.8846 9.64692 10.6033 9.90004 10.5752 10.2657L10.4064 12.7407C10.3783 13.0782 10.6314 13.3875 10.9971 13.4157C11.0252 13.4157 11.0252 13.4157 11.0533 13.4157C11.3908 13.4157 11.6721 13.1625 11.6721 12.825L11.8408 10.35C11.8408 9.98442 11.5877 9.70317 11.2502 9.67504Z"
+                            fill=""
+                          />
+                          <path
+                            d="M6.72245 9.67504C6.38495 9.70317 6.1037 10.0125 6.13182 10.35L6.3287 12.825C6.35683 13.1625 6.63808 13.4157 6.94745 13.4157C6.97558 13.4157 6.97558 13.4157 7.0037 13.4157C7.3412 13.3875 7.62245 13.0782 7.59433 12.7407L7.39745 10.2657C7.39745 9.90004 7.08808 9.64692 6.72245 9.67504Z"
+                            fill=""
+                          />
+                        </svg>
+                      </button>
+                      <button className="hover:text-primary">
+                        <svg
+                          className="fill-current"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M16.8754 11.6719C16.5379 11.6719 16.2285 11.9531 16.2285 12.3187V14.8219C16.2285 15.075 16.0316 15.2719 15.7785 15.2719H2.22227C1.96914 15.2719 1.77227 15.075 1.77227 14.8219V12.3187C1.77227 11.9812 1.49102 11.6719 1.12539 11.6719C0.759766 11.6719 0.478516 11.9531 0.478516 12.3187V14.8219C0.478516 15.7781 1.23789 16.5375 2.19414 16.5375H15.7785C16.7348 16.5375 17.4941 15.7781 17.4941 14.8219V12.3187C17.5223 11.9531 17.2129 11.6719 16.8754 11.6719Z"
+                            fill=""
+                          />
+                          <path
+                            d="M8.55074 12.3469C8.66324 12.4594 8.83199 12.5156 9.00074 12.5156C9.16949 12.5156 9.31012 12.4594 9.45074 12.3469L13.4726 8.43752C13.7257 8.1844 13.7257 7.79065 13.5007 7.53752C13.2476 7.2844 12.8539 7.2844 12.6007 7.5094L9.64762 10.4063V2.1094C9.64762 1.7719 9.36637 1.46252 9.00074 1.46252C8.66324 1.46252 8.35387 1.74377 8.35387 2.1094V10.4063L5.40074 7.53752C5.14762 7.2844 4.75387 7.31252 4.50074 7.53752C4.24762 7.79065 4.27574 8.1844 4.50074 8.43752L8.55074 12.3469Z"
+                            fill=""
+                          />
+                        </svg>
+                      </button> */}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BannerTable;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,137 +15,147 @@ import Alerts from '../pages/UiElements/Alerts';
 import Buttons from '../pages/UiElements/Buttons';
 import SingleProduct from '../pages/AddProduct/SingleProduct';
 import BulkUpload from '../pages/AddProduct/BulkUpload';
+import Banner from '../pages/Banner';
 
 const RoutesView = () => {
-    return (
-        <Routes>
-            <Route
-                index
-                element={
-                    <>
-                        <PageTitle title="Dashboard | Store Panel" />
-                        <ECommerce />
-                    </>
-                }
-            />
-            <Route
-                path="/calendar"
-                element={
-                    <>
-                        <PageTitle title="Calendar | Store Panel" />
-                        <Calendar />
-                    </>
-                }
-            />
-            <Route
-                path="/profile"
-                element={
-                    <>
-                        <PageTitle title="Profile | Store Panel" />
-                        <Profile />
-                    </>
-                }
-            />
-            <Route
-                path="/forms/form-elements"
-                element={
-                    <>
-                        <PageTitle title="Form Elements | Store Panel" />
-                        <FormElements />
-                    </>
-                }
-            />
-            <Route
-                path="/forms/form-layout"
-                element={
-                    <>
-                        <PageTitle title="Form Layout | Store Panel" />
-                        <FormLayout />
-                    </>
-                }
-            />
-            <Route
-                path="/products"
-                element={
-                    <>
-                        <PageTitle title="Products | Store Panel" />
-                        <Products />
-                    </>
-                }
-            />
-            <Route
-                path="/single-product/"
-                element={
-                    <>
-                        <PageTitle title="Single Products | Store Panel" />
-                        <SingleProduct />
-                    </>
-                }
-            />
-            <Route
-                path="/bulk-products/"
-                element={
-                    <>
-                        <PageTitle title="Bulk-Upload Products | Store Panel" />
-                        <BulkUpload />
-                    </>
-                }
-            />
-            <Route
-                path="/settings"
-                element={
-                    <>
-                        <PageTitle title="Settings | Store Panel" />
-                        <Settings />
-                    </>
-                }
-            />
-            <Route
-                path="/chart"
-                element={
-                    <>
-                        <PageTitle title="Basic Chart | Store Panel" />
-                        <Chart />
-                    </>
-                }
-            />
-            <Route
-                path="/ui/alerts"
-                element={
-                    <>
-                        <PageTitle title="Alerts | Store Panel" />
-                        <Alerts />
-                    </>
-                }
-            />
-            <Route
-                path="/ui/buttons"
-                element={
-                    <>
-                        <PageTitle title="Buttons | Store Panel" />
-                        <Buttons />
-                    </>
-                }
-            />
-            <Route
-                path="/auth/signin"
-                element={
-                    <>
-                        <PageTitle title="Signin | Store Panel" />
-                        <SignIn />
-                    </>
-                }
-            />
-            <Route
-                path="/auth/signup"
-                element={
-                    <>
-                        <PageTitle title="Signup | Store Panel" />
-                        <SignUp />
-                    </>
-                }
-            />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route
+        index
+        element={
+          <>
+            <PageTitle title="Dashboard | Store Panel" />
+            <ECommerce />
+          </>
+        }
+      />
+      <Route
+        path="/calendar"
+        element={
+          <>
+            <PageTitle title="Calendar | Store Panel" />
+            <Calendar />
+          </>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <>
+            <PageTitle title="Profile | Store Panel" />
+            <Profile />
+          </>
+        }
+      />
+      <Route
+        path="/forms/form-elements"
+        element={
+          <>
+            <PageTitle title="Form Elements | Store Panel" />
+            <FormElements />
+          </>
+        }
+      />
+      <Route
+        path="/forms/form-layout"
+        element={
+          <>
+            <PageTitle title="Form Layout | Store Panel" />
+            <FormLayout />
+          </>
+        }
+      />
+      <Route
+        path="/products"
+        element={
+          <>
+            <PageTitle title="Products | Store Panel" />
+            <Products />
+          </>
+        }
+      />
+      <Route
+        path="/single-product/"
+        element={
+          <>
+            <PageTitle title="Single Products | Store Panel" />
+            <SingleProduct />
+          </>
+        }
+      />
+      <Route
+        path="/bulk-products/"
+        element={
+          <>
+            <PageTitle title="Bulk-Upload Products | Store Panel" />
+            <BulkUpload />
+          </>
+        }
+      />
+      <Route
+        path="/banners"
+        element={
+          <>
+            <PageTitle title="Banner | Store Panel" />
+            <Banner />
+          </>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <>
+            <PageTitle title="Settings | Store Panel" />
+            <Settings />
+          </>
+        }
+      />
+      <Route
+        path="/chart"
+        element={
+          <>
+            <PageTitle title="Basic Chart | Store Panel" />
+            <Chart />
+          </>
+        }
+      />
+      <Route
+        path="/ui/alerts"
+        element={
+          <>
+            <PageTitle title="Alerts | Store Panel" />
+            <Alerts />
+          </>
+        }
+      />
+      <Route
+        path="/ui/buttons"
+        element={
+          <>
+            <PageTitle title="Buttons | Store Panel" />
+            <Buttons />
+          </>
+        }
+      />
+      <Route
+        path="/auth/signin"
+        element={
+          <>
+            <PageTitle title="Signin | Store Panel" />
+            <SignIn />
+          </>
+        }
+      />
+      <Route
+        path="/auth/signup"
+        element={
+          <>
+            <PageTitle title="Signup | Store Panel" />
+            <SignUp />
+          </>
+        }
+      />
+    </Routes>
+  );
+};
 export default RoutesView;
